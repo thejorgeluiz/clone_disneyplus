40d4d549ae06d95cf1d41e506b820f1f5327eabe
jorgeluiz <thejljorgeluiz@gmail.com>
2025-02-16 16:53:51 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultarElementoDoHeader();
+        } else {
+            exibeElementoDoHeader();
+        }
+    })
+    
+    //seção de atraçôes, programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -9,10 +23,31 @@ document.addEventListener('DOMContentLoaded', function(){
             escondeTodasAbas();
             aba.classList.add('shows__list--is-active')
             removeBotaoAtivo()
-            botao.target.classList.add('shows__tabs__button--is-active')
+            botao.target.classList.add('shows__tabs__button--is-active');
     })
     }
+    //seção FAQ, accordion
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+
+function ocultarElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementoDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
